3eb5f746535f4e4c1f396cc9ebe6393a8b322bf3
Jose <zaragoza.jose@gmail.com>
2022-10-22 14:09:36 -0700

    infoSection

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transaction with zero fees",
+  description:
+    "get aacces to our app that allows you to send unlimited tans without getting charges fess.",
+  buttonLabel: "get Started",
+  imgStart: false,
+  img: require("../../images/svg-1.svg").default,
+  alt: "car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
